5fee31a75e6971fe011771245229366ad0f9669b
NHarmes <nharmes@gmail.com>
2025-03-11 15:09:39 +0000

    updated main.py to limit framerate

--- main.py
+++ main.py
@@ -8,13 +8,23 @@ from constants import *
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+
+    #pygame.time.Clock
+    clock = pygame.time.Clock()
+    dt = 0
+
     while True:
+        #check for window being closed by user
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+            
+        #fill screen area in black
         screen.fill(000)
         pygame.display.flip()
+
+        #limit framrate to 60fps
+        dt = clock.tick(60)/1000
     
     
     
