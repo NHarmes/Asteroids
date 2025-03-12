d6d38c08ea6bd49796d01fbc49792979f3fa1c4b
NHarmes <nharmes@gmail.com>
2025-03-12 10:42:56 +0000

    added player movement

--- main.py
+++ main.py
@@ -29,6 +29,9 @@ def main():
         #fill screen area in black
         screen.fill(000)
 
+        #limit framrate to 60fps
+        dt = clock.tick(60)/1000
+
         #insert rotate player functionality
         player.update(dt)
 
@@ -41,7 +44,7 @@ def main():
         pygame.display.flip()
 
         #limit framrate to 60fps
-        dt = clock.tick(60)/1000
+        #dt = clock.tick(60)/1000
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED, PLAYER_SPEED
 from circleshape import *
 
 class Player(CircleShape):
@@ -23,6 +23,11 @@ class Player(CircleShape):
     def rotate(self, dt):
         self.rotation += (PLAYER_TURN_SPEED * dt)
 
+    #move the player fore/aft
+    def move(self, dt):
+        forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        self.position += forward * PLAYER_SPEED * dt
+
     #copy pasta method from bootdev
     def update(self, dt):
         keys = pygame.key.get_pressed()
@@ -32,3 +37,10 @@ class Player(CircleShape):
 
         if keys[pygame.K_d]:
             self.rotate(dt)
+
+        if keys[pygame.K_w]:
+            self.move(dt)
+
+        if keys[pygame.K_s]:
+            self.move(-dt)
+    
