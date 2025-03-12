bb73e452e4346007c460c12d712c4bcc12e482e6
NHarmes <nharmes@gmail.com>
2025-03-12 10:28:58 +0000

    Added player rotation

--- main.py
+++ main.py
@@ -16,6 +16,7 @@ def main():
     x = SCREEN_WIDTH/2
     y = SCREEN_HEIGHT/2
     
+    #instantiate Player object and store it in a variable
     player = Player(x, y)
        
 
@@ -28,19 +29,19 @@ def main():
         #fill screen area in black
         screen.fill(000)
 
+        #insert rotate player functionality
+        player.update(dt)
+
         #draw player here
         player.draw(screen)   
 
-
+        
+        
 
         pygame.display.flip()
 
         #limit framrate to 60fps
         dt = clock.tick(60)/1000
-    
-    
-    
-    
 
 
 if __name__ == "__main__":
--- player.py
+++ player.py
@@ -1,4 +1,4 @@
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 from circleshape import *
 
 class Player(CircleShape):
@@ -17,4 +17,18 @@ class Player(CircleShape):
     
     #Override 'draw' method of circleshape
     def draw(self, screen):
-        pygame.draw.polygon(screen, 111, self.triangle(), 2)
+        pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    #rotate method
+    def rotate(self, dt):
+        self.rotation += (PLAYER_TURN_SPEED * dt)
+
+    #copy pasta method from bootdev
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
